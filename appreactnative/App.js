f75e957a0a778722fba9c0807fbdb42cee3ad826
Android Studio <devandroid@ort>
2025-05-09 10:01:47 -0300

    terminado

--- appreactnative/App.js
+++ appreactnative/App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput, Image, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-    <TextInput
-      placeholder="Ingrese su Email"
+    <Image
+      style={styles.tinyLogo}
+      source={require('./assets/adaptive-icon.png')}
     />
+    <StatusBar style="dark"/>
     <TextInput
+          style={styles.input}
+          placeholder="Ingrese su Usuario"
+        />
+    <TextInput
+    style={styles.input}
       placeholder="Ingrese su Contraseña"
       secureTextEntry
     />
     <Button title="INGRESAR" onPress={() => console.log('Login')} />
-    <StatusBar style="light" />
+    <Text style={styles.text}>Olvidé mi contraseña</Text>
+    
   </View>
   );
 }
@@ -24,4 +32,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  text:{
+    color: 'red',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  
+  }
 });
+
